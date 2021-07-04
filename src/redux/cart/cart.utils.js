579cc8fd8925d2ef7053798dcf353e1b969c7f18
Igor2075 <rogier.van.amersfoort.75@gmail.com>
2021-07-03 22:39:41 -0300

    add and remove items from checkout page

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -12,3 +12,20 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    console.log(cartItemToRemove);
+    if (cartItemToRemove.quantity > 1) {
+        return cartItems.map((cartItem) =>
+            cartItem.quantity > 1
+                ? { ...cartItem, quantity: cartItem.quantity - 1 }
+                : cartItemToRemove
+        );
+    }
+
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+};
+
+export const clearItemFromCart = (cartItems, cartItemToRemove) => {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+};
